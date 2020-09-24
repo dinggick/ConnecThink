1f2dde1e6c609e489881e5919a410f081a1efb23
DESKTOP-4QNPDLJ\Hailey <yi.hailey@gmail.com>
2020-09-24 10:20:08 +0900

    no message

--- connecthink/src/com/connecthink/controller/ProjectController.java
+++ connecthink/src/com/connecthink/controller/ProjectController.java
@@ -2,45 +2,75 @@ package com.connecthink.controller;
 
 import java.util.List;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.connecthink.entity.Project;
 import com.connecthink.service.ProjectService;
 
-@RestController
+@Controller
 public class ProjectController {
 	
 	@Autowired
 	private ProjectService service;
 	
 	@PostMapping(value="/manageMyProject")
+	@ResponseBody
 	public List<Project> manageMyProject(Integer managerNo) {
-		System.out.println("manageMyProject 호출");
 		return service.findByManagerNo(managerNo);
 	}
 	
-	@PostMapping(value="/manageMyApplication")
-	public List<Project> manageMyApplication(Integer memberNo) {
-		List<Project> pList = service.findMyApplication(memberNo);
-		return pList;
-	}
-
-	@PostMapping(value="/manageMyInvitation")
-	public List<Project> manageMyInvitation(Integer memberNo) {
-		List<Project> pList = service.findMyInvitation(memberNo);
-		return pList;
+	/**
+	 * @author 임수정
+	 * manageTeam.jsp 보여주기
+	 */
+	@RequestMapping("/manageTeam")
+	public void manageTeam() {
+		
 	}
 	
-	@PostMapping(value="/manageInvited")
-	public List<Project> manageInvited(Integer managerNo) {
-		List<Project> pList = service.findInvited(managerNo);
-		return pList;
+	/**
+	 * @author 임수정
+	 * 내 지원 목록 보기
+	 */
+	@PostMapping(value="/manageTeam/myApplication")
+	@ResponseBody
+	public List<Project> MyApplication(Integer memberNo) {
+		return service.findMyApplication(memberNo);
 	}
+
+	/**
+	 * @author 임수정
+	 * 내가 받은 초대 목록 보기
+	 */
+	@PostMapping(value="/manageTeam/myInvitation")
+	@ResponseBody
+	public List<Project> MyInvitation(Integer memberNo) {
+		return service.findMyInvitation(memberNo);
+	}
+	
+	/**
+	 * @author 임수정
+	 * 내 프로젝트에 초대했던 멤버 목록 보기
+	 */
+	@PostMapping(value="/manageTeam/invited")
+	@ResponseBody
+	public List<Project> Invited(Integer managerNo) {
+		return service.findInvited(managerNo);
+	}
+<<<<<<< HEAD
 	////
 	@PostMapping(value="/manageApplied")
 	public List<Project> manageApplied(Integer managerNo) {
@@ -49,6 +79,17 @@ public class ProjectController {
 		System.out.println(pList);
 		System.out.println("지원자 찾기 서비스 끝");
 		return pList;
+=======
+	
+	/**
+	 * @author 임수정
+	 * 내 프로젝트에 지원한 멤버 목록 보기
+	 */
+	@PostMapping(value="/manageTeam/applied")
+	@ResponseBody
+	public List<Project> Applied(Integer managerNo) {
+		return service.findApplied(managerNo);
+>>>>>>> fec1f418828f024ae3e917aebf2788169d16fca8
 	}
 	@PostMapping(value="/memberModal")
 	public List<Project> memberModal(HttpServletRequest request) {
